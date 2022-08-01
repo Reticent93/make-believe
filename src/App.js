3fdc30e8acdce9127693dd5695e488fed0674495
gren8 <greg.renfro93@gmail.com>
2022-08-01 09:41:26 -0700

    fb login

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ function App() {
   return (
     <div className="App">
     <Routes>
-      <Route path="/" index element={<Home />} 
-      />
-      <Route path='/login' element={<Login />} />
+      <Route index element={<Home />} />
+      <Route path="home" element={<Home />}>
+      <Route path='login' element={<Login />} />
+      </Route>
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
