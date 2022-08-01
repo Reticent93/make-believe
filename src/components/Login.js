7cd8744435b0d327fbaa8522a451b81c6847ea63
gren8 <greg.renfro93@gmail.com>
2022-08-01 09:01:11 -0700

    fb login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,66 +2,34 @@ import {useEffect} from 'react'
 
 const Login = () => {
 
-    function statusChangeCallback(response) {
-        console.log('statusChangeCallback');
-        console.log(response);
-        // The response object is returned with a status field that lets the
-        // app know the current login status of the person.
-        // Full docs on the response object can be found in the documentation
-        // for FB.getLoginStatus().
-        if (response.status === 'connected') {
-            // Logged into your app and Facebook.
-            console.log('Welcome!  Fetching your information.... ');
-            FB.api('/me', function (response) {
-                console.log('Successful login for: ' + response.name);
-                document.getElementById('status').innerHTML =
-                  'Thanks for logging in, ' + response.name + '!';
-            });
-        } else {
-            // The person is not logged into your app or we are unable to tell.
-            document.getElementById('status').innerHTML = 'Please log ' +
-              'into this app.';
-        }
-    }
 
     useEffect(() => {
-       /* global FB */
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1581438475584268',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v14.0'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
-  
-
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
+        window.fbAsyncInit = () => {
+            window.FB.init({
+                appId            : '1581438475584268',
+                autoLogAppEvents : true,
+                xfbml            : true,
+                version          : 'v14.0'
+            });
+        };
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
     }, []);
 
-   
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+    const onLoginClick = () => {
+        window.FB.login();
+    };
   return (
     <>
     <div>
         <h1>Login</h1>
         <div id='status' className="fb-login-button" data-width="" data-size="medium" data-button-type="login_with"
-    data-layout="default" data-auto-logout-link="true" data-use-continue-as="false" onClick={checkLoginState} />
+    data-layout="default" data-auto-logout-link="true" data-use-continue-as="false" onClick={onLoginClick} />
     </div>
     </>
   )
