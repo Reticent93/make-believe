085bd01feb479aa870fcad7b144cdb7862dff939
gren8 <greg.renfro93@gmail.com>
2022-08-02 08:01:33 -0700

    fb login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import {useEffect} from 'react'
 
 const Login = () => {
 
-
+let accessToken;
     useEffect(() => {
         window.fbAsyncInit = () => {
             window.FB.init({
@@ -14,26 +14,12 @@ const Login = () => {
         };
         /* global FB */
         FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-              // The user is logged in and has authenticated your
-              // app, and response.authResponse supplies
-              // the user's ID, a valid access token, a signed
-              // request, and the time the access token 
-              // and signed request each expire.
               var uid = response.authResponse.userID;
-              var accessToken = response.authResponse.accessToken;
+              var accessToken = response.authResponse;
               console.log('logged in', accessToken)
                 console.log('logged in still', uid)
-            } else if (response.status === 'not_authorized') {
-              // The user hasn't authorized your application.  They
-              // must click the Login button, or you must call FB.login
-              // in response to a user gesture, to launch a login dialog.
-            } else {
-              // The user isn't logged in to Facebook. You can launch a
-              // login dialog with a user gesture, but the user may have
-              // to log in to Facebook before authorizing your application.
-            }
-           }, true);
+         
+      }, true);
         (function (d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) { return; }
@@ -47,13 +33,19 @@ const Login = () => {
         window.FB.login();
         console.log('clicked', window.FB.login);
     };
+    const onLogoutClick = () => {
+        window.FB.logout();
+    }
   return (
     <>
     <div>
         <h1>Login</h1>
+        {accessToken ? 
         <div id='status' className="fb-login-button" data-width="" data-size="medium" data-button-type="login_with"
-    data-layout="default" data-auto-logout-link="true" data-use-continue-as="false" onClick={onLoginClick} />
-    </div>
+    data-layout="default" data-auto-logout-link="true" data-use-continue-as="false" onClick={onLoginClick} /> :
+    <div onClick={onLogoutClick}/> }
+    </div> 
+
     </>
   )
 }
